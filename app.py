1df97fbbc9cf4a9a3005db223fe6340c62260c40
effervescent000 <kislio01@gmail.com>
2022-02-04 12:00:23 -0500

    basic models + some schema

--- app.py
+++ app.py
@@ -7,7 +7,70 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+
+class Recipe(db.Model):
+    __tablename__ = "recipes"
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(200), nullable=False)
+    servings = db.Column(db.Integer)
+    image_url = db.Column(db.Text)
+    instructions = db.Column(db.Text)
+
+    ingredients = db.relationship("Ingredient", backref="recipe", lazy=True, cascade="all, delete-orphan")
+    times = db.relationship("Time", backref="recipe", lazy=True, cascade="all, delete-orphan")
+
+
+class Ingredient(db.Model):
+    __tablename__ = "ingredients"
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(200), nullable=False)
+    quantity = db.Column(db.String(200), nullable=False)
+
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipes.id"), nullable=False)
+
+
+class Time(db.Model):
+    __tablename__ = "times"
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    prep = db.Column(db.Integer)
+    cook = db.Column(db.Integer)
+    active = db.Column(db.Integer)
+    inactive = db.Column(db.Integer)
+    ready = db.Column(db.Integer)
+    total = db.Column(db.Integer)
+
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipes.id"), nullable=False)
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "username", "password")
+
+
+one_user_schema = UserSchema()
+multi_user_schema = UserSchema(many=True)
+
+
+class IngredientSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "quantity", "recipe_id")
+
+
+one_ingredient_schema = IngredientSchema()
+multi_ingredient_schema = IngredientSchema(many=True)
+
+
+class RecipeSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "servings", "image_url", "instructions")
